798aea966e05affb03af599657c8453316baa626
purnesh <48253358+purneshkotian@users.noreply.github.com>
2024-10-12 20:51:53 +0530

    ui changes

--- task-management-frontend/src/pages/CreateTask.jsx
+++ task-management-frontend/src/pages/CreateTask.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { TextField, Button, Typography } from "@mui/material";
 import axios from "../services/api";
+import Navbar from "../components/NavBar";
 
 const CreateTask = () => {
   const [task, setTask] = useState({ title: "", description: "" });
@@ -8,6 +9,7 @@ const CreateTask = () => {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setTask({ ...task, [name]: value });
+    console.log(task);
   };
 
   const handleSubmit = (e) => {
@@ -18,36 +20,39 @@ const CreateTask = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <Typography variant="h4" className="mb-4">
-        Create New Task
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Title"
-          name="title"
-          value={task.title}
-          onChange={handleInputChange}
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Description"
-          name="description"
-          value={task.description}
-          onChange={handleInputChange}
-          fullWidth
-          margin="normal"
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          className="mt-4"
-        >
-          Create Task
-        </Button>
-      </form>
+    <div>
+      <Navbar />
+      <div className="container mx-auto p-4">
+        <Typography variant="h4" className="mb-4">
+          Create New Task
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Title"
+            name="title"
+            value={task.title}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Description"
+            name="description"
+            value={task.description}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className="mt-4"
+          >
+            Create Task
+          </Button>
+        </form>
+      </div>
     </div>
   );
 };
